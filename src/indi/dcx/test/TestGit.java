157e19641a9fdac624ed7047fff389e536fac777
233ddeeff <2533766383@qq.com>
2020-03-20 14:40:51 +0800

    测试3

--- src/indi/dcx/test/TestGit.java
+++ src/indi/dcx/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
   public static void main(String[] args) {
 	System.out.println("Test GIT HUB");
          System.out.println("Test GIT HUB2");
+          System.out.println("Test GIT HUB3");
 }
 }
